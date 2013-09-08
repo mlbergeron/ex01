342f9e8c35f107fc625d4335f3f74b2992752a2c
mlbergeron <martine.bergeron.4@ulaval.ca>
2013-09-08 16:10:20 -0400

    Apres verification des standards

--- bin/ex01.dart
+++ bin/ex01.dart
@@ -1,8 +1,9 @@
-/*Write a program that prints the numbers from 1 to 100. 
+/**
+ * Write a program that prints the numbers from 1 to 100. 
  * But for multiples of three print “Fizz” instead of the number 
  * and for the multiples of five print “Buzz”. 
  * For numbers which are multiples of both three and five print “FizzBuzz”.
-*/
+ */
 
 const DIVISIBLE3 = 3;
 const DIVISIBLE5 = 5;
@@ -12,43 +13,31 @@ void main() {
   String reponse;
   
   print("Résultats de FizzBuzz\n");
-  for (int nombre = 1; nombre <= 100; nombre++) 
-  {
+  for (int nombre = 1; nombre <= 100; nombre++) {
     reponse = determineFizzBuzz(nombre); 
     print("Pour le nombre $nombre, la réponse est : $reponse");
   }
 }
 
-/* determineFizzBuzz
- * Si le nombre est un multiple de 3, retourne "Fizz"
- * Si le nombre est un multiple de 5, retourne "Buzz"
- * Si le nombre est un multiple de 3 et de 5, retourne "FizzBuzz"
- * Si le nombre n'est ni un multiple de 3, ni un multiple de 5, retourne le nombre
- *   
- * input : 
- *    nombre : pour lequel on veut évaluer la chaîne FizzBuzz
- * output :
- *    La chaîne résultante    
- * */
-String determineFizzBuzz(int nombre){
+/** 
+ * Détermine la valeur FizzBuzz à retourner selon le nombre [nombre].
+ * La valeur sera déterminée comme suit :
+ * Si le nombre [nombre] est un multiple de 3, retourne "Fizz".
+ * Si le nombre [nombre] est un multiple de 5, retourne "Buzz".
+ * Si le nombre [nombre] est un multiple de 3 et de 5, retourne "FizzBuzz".
+ * Si le nombre [nombre] n'est ni un multiple de 3, ni un multiple de 5, retourne le nombre [nombre] lui-même.
+ */
+String determineFizzBuzz(int nombre) {
   String reponse = "";
   
   // Si le nombre se divise par 3, sans reste, on concatène 'Fizz'
-  if (nombre%DIVISIBLE3 == 0)
-  {  
-    reponse = reponse + "Fizz";
-  }
+  if (nombre%DIVISIBLE3 == 0) reponse = reponse + "Fizz";
+ 
+   // Si le nombre se divise par 5, sans reste, on concatène 'Buzz'
+  if (nombre%DIVISIBLE5 == 0) reponse = reponse + "Buzz";
   
-  // Si le nombre se divise par 5, sans reste, on concatène 'Buzz'
-  if (nombre%DIVISIBLE5 == 0)
-  { 
-    reponse = reponse + "Buzz";
-  }
-  
-  if (reponse == "")
-  {  
-    reponse = nombre.toString();
-  }
+  if (reponse == "") reponse = nombre.toString();
+ 
 
   return reponse;
 }
