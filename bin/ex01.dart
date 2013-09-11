49ff0ebec3888959d34abf19ccffe3f6f6950a6f
Dzenan Ridjanovic <dzenanr@gmail.com>
2013-09-11 15:02:26 -0400

    add doc; format code

--- bin/ex01.dart
+++ bin/ex01.dart
@@ -1,25 +1,23 @@
 /**
- * Write a program that prints the numbers from 1 to 100. 
- * But for multiples of three print “Fizz” instead of the number 
- * and for the multiples of five print “Buzz”. 
+ * Write a program that prints the numbers from 1 to 100.
+ * But for multiples of three print “Fizz” instead of the number
+ * and for the multiples of five print “Buzz”.
  * For numbers which are multiples of both three and five print “FizzBuzz”.
  */
-
 const DIVISIBLE3 = 3;
 const DIVISIBLE5 = 5;
 
 void main() {
-
   String reponse;
-  
+
   print("Résultats de FizzBuzz\n");
   for (int nombre = 1; nombre <= 100; nombre++) {
-    reponse = determineFizzBuzz(nombre); 
+    reponse = determineFizzBuzz(nombre);
     print("Pour le nombre $nombre, la réponse est : $reponse");
   }
 }
 
-/** 
+/**
  * Détermine la valeur FizzBuzz à retourner selon le nombre [nombre].
  * La valeur sera déterminée comme suit :
  * Si le nombre [nombre] est un multiple de 3, retourne "Fizz".
@@ -29,15 +27,10 @@ void main() {
  */
 String determineFizzBuzz(int nombre) {
   String reponse = "";
-  
   // Si le nombre se divise par 3, sans reste, on concatène 'Fizz'
-  if (nombre%DIVISIBLE3 == 0) reponse = reponse + "Fizz";
- 
+  if (nombre % DIVISIBLE3 == 0) reponse = reponse + "Fizz";
    // Si le nombre se divise par 5, sans reste, on concatène 'Buzz'
-  if (nombre%DIVISIBLE5 == 0) reponse = reponse + "Buzz";
-  
+  if (nombre % DIVISIBLE5 == 0) reponse = reponse + "Buzz";
   if (reponse == "") reponse = nombre.toString();
- 
-
   return reponse;
 }
